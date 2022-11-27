9257192b53060b34b315f7aea81417e2a8b5fde3
Jefferson Santos <jeff-ds@outlook.com>
2022-11-27 20:17:47 -0300

    resolução do tópico 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-do-dia.js
@@ -0,0 +1,25 @@
+/*
+TÓPICO 1:
+Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:  
+
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b)
+*/
+
+// Resolução - Tópico 1:
+let num1 = 8;
+let num2 = 3;
+const adicao = num1 + num2;
+const subtracao = num1 - num2;
+const multiplicacao = num1 * num2;
+const divisao = num1 / num2;
+const modulo = num1 % num2;
+
+console.log('Adição: ' + adicao);
+console.log('Subtração: ' + subtracao);
+console.log('Multiplicação: ' + multiplicacao);
+console.log('Divisão: ' + divisao);
+console.log('Módulo: ' + modulo);
