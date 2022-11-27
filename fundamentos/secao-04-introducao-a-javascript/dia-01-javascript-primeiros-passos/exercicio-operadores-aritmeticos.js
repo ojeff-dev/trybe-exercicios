9b84f0860dfbfb05dcaed51dd6feed169f294f1a
Jefferson Santos <jeff-ds@outlook.com>
2022-11-27 16:41:42 -0300

    Resolução do exercício 'para fixar' - operadores aritméticos'

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-operadores-aritmeticos.js
@@ -0,0 +1,13 @@
+// Exercício "Para Fixar" 2 (Operadores Aritméticos):
+
+//Rectangle:
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log('Base: ' + base + ';', 'Heigth: ' + heigth + ';', 'Area: ' + area);
+
+const perimeter = (base * 2) + (heigth * 2);
+
+console.log('The perimeter is: ' + perimeter);
