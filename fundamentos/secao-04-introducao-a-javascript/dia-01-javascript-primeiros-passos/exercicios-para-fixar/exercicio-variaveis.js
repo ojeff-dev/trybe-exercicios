3abdf7bf31de88d904438cada08899b2499f65ed
Jefferson Santos <jeff-ds@outlook.com>
2022-11-27 16:39:11 -0300

    Resolução do exercício 'para fixar' - variáveis'

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicios-para-fixar/exercicio-variaveis.js
@@ -0,0 +1,14 @@
+// Exercício "Para Fixar" 1 (Variáveis):
+
+const myName = 'Jefferson';
+const birthCity = 'São João de Meriti';
+let birthYear = 1997;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = 'Rio de Janeiro';
+console.log(birthCity);
+// O erro acontece porque não é possível alterar o valor atribuído a uma variável constante.
