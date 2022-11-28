b9904a609af99e6c0de99cf6436774ce887b43ed
Jefferson Santos <jeff-ds@outlook.com>
2022-11-28 17:59:36 -0300

    Resolução do exercício 'para fixar' - Arrays

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-para-fixar/exercicios-arrays.js
@@ -0,0 +1,21 @@
+// Obtenha o valor “Serviços” do array menu:
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Procure o índice do valor “Portfólio” do array menu:
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// Adicione o valor “Contato” no final do array menu:
+// De olho na dica 👀: Use o método .push()
+
+menu.push('Contato');
+
+console.log(menu);
+
+// FIM!
