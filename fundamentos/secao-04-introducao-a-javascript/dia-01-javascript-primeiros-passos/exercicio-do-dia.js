5bd78df46934547823fe0001bc9ba4551ce7632a
Jefferson Santos <jeff-ds@outlook.com>
2022-11-27 23:42:54 -0300

    resolução do tópico 7

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-do-dia.js
@@ -170,3 +170,36 @@ switch(pecaDeXadrez.toLowerCase()) {
 console.log('');
 console.log('*********************************')
 console.log('');
+/* 
+TÓPICO 7:
+Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+- Porcentagem >= 90 -> A
+- Porcentagem >= 80 -> B
+- Porcentagem >= 70 -> C
+- Porcentagem >= 60 -> D
+- Porcentagem >= 50 -> E
+- Porcentagem < 50 -> F
+- O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+*/
+const nota = 96;
+
+if(nota >= 90 && nota <= 100) {
+  console.log('Conceito: A');
+} else if(nota >= 80) {
+  console.log('Conceito: B');
+} else if(nota >= 70) {
+  console.log('Conceito: C');
+} else if(nota >= 60) {
+  console.log('Conceito: D');
+} else if(nota >= 50) {
+  console.log('Conceito: E');
+} else if(nota >= 0) {
+  console.log('Conceito: F');
+} else if(nota < 0 || nota > 100) {
+  console.log('ERRO! O valor deve ser um número entre 0 e 100');
+} else {
+  console.log('Inválido!');
+}
+console.log('');
+console.log('*********************************')
+console.log('');
