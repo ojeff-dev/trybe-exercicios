0de07e44c2aea1ac610e66502e1ef354b260b0c5
Jefferson Santos <jeff-ds@outlook.com>
2022-11-30 15:21:55 -0300

    resolução do tópico 1 - para-fixar

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-objetos.js
@@ -0,0 +1,24 @@
+/* 
+TÓPICO 1:
+
+Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+ - let name = 'Marta';
+ - let lastName = 'Silva';
+ - let age = 34;
+ - let medals = { golden: 2, silver: 3 };
+*/
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silvia',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+};
+console.log(player);
+
+console.log('');
+console.log('****************************************');
+console.log('');
