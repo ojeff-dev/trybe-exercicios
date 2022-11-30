8311a050ba15d22347da308b4f1951774710ea3b
Jefferson Santos <jeff-ds@outlook.com>
2022-11-29 22:34:55 -0300

    resolução do tópico 3 - Tamanho das palavras

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -37,3 +37,32 @@ let word = 'tryber';
 console.log('');
 console.log('*********************************');
 console.log('');
+/* 
+TÓPICO 3:
+
+Considere o array de strings abaixo:
+  - let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+  - Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
+*/
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for(let index = 0; index < array.length; index += 1) {
+
+  if(array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+  }
+  if(array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+  }
+  
+}
+console.log('A maior palavra' + ' é: ' + maiorPalavra + ' e a menor palavra é: ' + menorPalavra);
+
+console.log('');
+console.log('*********************************');
+console.log('');
