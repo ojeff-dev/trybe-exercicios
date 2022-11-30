04c9356dee9a04a8644f3fa334fe8f6be12953bf
Jefferson Santos <jeff-ds@outlook.com>
2022-11-29 21:22:59 -0300

    resolução do tópico 2 - Palavra Invertida

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -16,3 +16,24 @@ for(let index = 1; index <= numeroEscolhido; index += 1){
   multiplicador *= index;
 }
 console.log('O fatorial de ' + numeroEscolhido + ' é: ' + multiplicador);
+
+console.log('');
+console.log('*********************************');
+console.log('');
+/* 
+TÓPICO 2:
+Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+*/
+ let word = 'jefferson';
+ let palavraInvertida = '';
+
+ for(let index = word.length - 1; index < word.length && index >= 0; index -= 1) {
+  palavraInvertida += word[index];
+ }
+ console.log(palavraInvertida);
+
+console.log('');
+console.log('*********************************');
+console.log('');
