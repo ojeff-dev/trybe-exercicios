254374922205980237250f918fa555c1468eeb19
Jefferson Santos <jeff-ds@outlook.com>
2022-11-30 22:39:32 -0300

    resolução do tópico 5 - para-fixar

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-funcoes.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-funcoes.js
@@ -104,3 +104,32 @@ console.log(numStatus(5));
 console.log('');
 console.log('****************************************');
 console.log('');
+
+/* 
+TÓPICO 5:
+
+Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+
+- Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+- Um ângulo será considerado inválido se não tiver um valor positivo.
+
+*/
+
+const angulo1 = 45;
+const angulo2 = 45;
+const angulo3 = 90;
+
+function verificaTriangulo(){
+  if(angulo1 <= 0 || angulo2 <= 0 || angulo3 <= 0){
+    return 'Erro: ângulo(s) menor(es) ou igual a zero.';
+  } else if(angulo1 + angulo2 + angulo3 === 180){
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(verificaTriangulo());
+
+console.log('');
+console.log('******************** FIM ********************');
+console.log('');
