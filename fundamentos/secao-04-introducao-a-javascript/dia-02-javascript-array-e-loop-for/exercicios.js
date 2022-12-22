92139db6d36896b20da61a089823e3684cf8bc55
Jefferson Santos <jeff-ds@outlook.com>
2022-12-21 21:06:19 -0300

    resolução do tópico 8

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -69,3 +69,9 @@ for (let index = 0; index < numbers.length; index += 1) {
   }
 }
 console.log(`${menorValor} é o menor valor do array`);
+
+/* 8. Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado; */
+
+for (let index = 1; index <= 25; index += 1) {
+  console.log(index);
+}
