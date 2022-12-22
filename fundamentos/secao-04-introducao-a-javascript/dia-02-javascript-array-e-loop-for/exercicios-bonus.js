b9adf35d359cc717f888fb5badcf2f3e8fe78220
Jefferson Santos <jeff-ds@outlook.com>
2022-12-21 21:36:45 -0300

    resolução do tópico 2 - exercícios bônus

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-bonus.js
@@ -16,3 +16,16 @@ for (let index = 1; index < numbers.length; index += 1) {
   }
 }
 console.log(numbers);
+
+/* 2. Ordene o array numbers em ordem decrescente e imprima seus valores; */
+
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let index2 = 0; index2 < index; index2 += 1) {
+    if (numbers[index] > numbers[index2]) {
+      let position = numbers[index];
+      numbers[index] = numbers[index2];
+      numbers[index2] = position;
+    }
+  }
+}
+console.log(numbers);
