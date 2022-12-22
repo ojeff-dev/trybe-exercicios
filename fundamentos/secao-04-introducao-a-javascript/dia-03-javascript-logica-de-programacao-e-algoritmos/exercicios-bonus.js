49d6bcb8e1dfd89972e297c2b5bbbe447232c733
Jefferson Santos <jeff-ds@outlook.com>
2022-12-21 23:05:19 -0300

    resolução do tópico 1 - exercícios bônus

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-bonus.js
@@ -0,0 +1,25 @@
+/* Exercícios - bônus */
+
+/* 
+Agora vamos trabalhar com algumas formas geométricas! Utilize a estrutura de repetição for para escrever os programas abaixo:
+*/
+
+/*
+1. Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+let asteriscos = '*';
+const n = 5;
+
+for (let index = 0; index < n; index += 1) {
+  console.log(asteriscos.repeat(n));
+}
