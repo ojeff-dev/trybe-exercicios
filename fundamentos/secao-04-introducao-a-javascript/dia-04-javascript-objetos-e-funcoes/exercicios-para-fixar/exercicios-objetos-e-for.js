6466d25d1597e921e508344cac2e968324c6a251
Jefferson Santos <jeff-ds@outlook.com>
2023-01-27 16:57:20 -0300

    tópico 5 - ok

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-objetos-e-for.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-objetos-e-for.js
@@ -73,3 +73,37 @@ for (let index in info) {
 }
 
 console.log('');
+
+/* 
+5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+Margarida e Tio Patinhas
+Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+Ambos recorrentes // Atenção para essa última linha!
+
+*/
+
+const infoObj1 = [];
+const infoObj2 = [];
+
+const info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let index in info) {
+  infoObj1.push(info[index]);
+}
+
+for (let index in info2) {
+  infoObj2.push(info2[index]);
+}
+
+for (let index = 0; index < infoObj1.length; index += 1) {
+  if (infoObj1[index] !== infoObj2[index]) {
+    console.log(`${infoObj1[index]} e ${infoObj2[index]}`);
+  }
+}
