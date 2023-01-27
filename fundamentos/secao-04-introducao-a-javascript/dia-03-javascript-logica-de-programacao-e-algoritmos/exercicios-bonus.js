ee7f90d97457a2eee4220f50957cd50dc1f2449e
Jefferson Santos <jeff-ds@outlook.com>
2023-01-27 15:49:35 -0300

    exercicios bonus - ok

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios-bonus.js
@@ -18,7 +18,7 @@ n = 5
 */
 
 let asteriscos = '*';
-const n = 5;
+let n = 5;
 
 for (let index = 0; index < n; index += 1) {
   console.log(asteriscos.repeat(n));
@@ -75,3 +75,34 @@ n = 5
 for (let index = n - 1; index >= 0; index -= 2) {
   console.log(' '.repeat(index / 2) + asteriscos.repeat(n - index));
 }
+
+console.log('');
+/* 
+5. Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+
+Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+n = 7
+
+   *
+  * *
+ *   *
+*******
+
+*/
+n = 7;
+
+for (let index = 1; index <= (n - 3); index += 1) {
+  const simbolo = '*';
+  const espaco = ' '.repeat((n - 3) - index);
+  const espacoMeio = index > 1 && index < (n - 3) ? ' '.repeat(2 * index - 3) : '';
+  
+  if (index === 1) {
+    console.log(espaco + simbolo + espaco);
+  } else if (index === (n - 3)) {
+    console.log(simbolo.repeat(n));
+  } else {
+    console.log(espaco + simbolo + espacoMeio + simbolo + espaco);
+  }
+
+}
+// EXERCÍCIO 5 AINDA NÃO FUNCIONA COM 'n' != 7.
