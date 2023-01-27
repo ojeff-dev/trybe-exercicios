32aad689fdc2ac088b248bc8fd05fd629b154bbc
Jefferson Santos <jeff-ds@outlook.com>
2023-01-27 16:55:01 -0300

    tópico 2 - ok

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-objetos-e-for.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-objetos-e-for.js
@@ -23,3 +23,21 @@ let info = {
 };
 
 console.log(`Bem-vinda, ${info.personagem}!`);
+
+console.log('');
+
+/* 
+2. Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+
+{
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+}
+
+*/
+
+info['recorrente'] = 'Sim';
+
+console.log(info);
