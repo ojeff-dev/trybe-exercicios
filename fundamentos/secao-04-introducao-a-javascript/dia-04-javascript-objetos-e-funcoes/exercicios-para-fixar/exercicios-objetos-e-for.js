c09ad8190379f423e6cb4345177c0ebacc5dacad
Jefferson Santos <jeff-ds@outlook.com>
2023-01-27 16:56:16 -0300

    tópico 4 - ok

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-objetos-e-for.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-para-fixar/exercicios-objetos-e-for.js
@@ -58,3 +58,18 @@ for (let index in info) {
 }
 
 console.log('');
+
+/* 
+4. Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+Margarida
+Pato Donald
+Namorada do personagem principal nos quadrinhos do Pato Donald
+Sim
+
+*/
+
+for (let index in info) {
+  console.log(info[index]);
+}
+
+console.log('');
