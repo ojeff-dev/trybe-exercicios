bac80d1dc0d31010b6d7dac934393d3f3dde61f1
Jefferson Santos <jeff-ds@outlook.com>
2023-01-30 20:01:04 -0300

    exercícios bonus 02 - ok

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-bonus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicios-bonus.js
@@ -58,3 +58,31 @@ const convertRoman = (romanNum) => {
 
 console.log(convertRoman('CM'));
 
+/* 
+
+Exercício 2
+- Crie uma função chamada arrayOfNumbers que receberá a variável vector como parâmetro.
+
+- Através de um loop for, percorra essa variável, busque os números pares e os adicione a um novo array, que deverá ser retornado ao final pela função.
+
+*/
+
+const vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+
+const arrayOfNumbers = (vector) => {
+  const pares = [];
+
+  for (let index = 0; index < vector.length; index += 1) {
+    const arrayVector = vector[index];
+    
+    for (let index2 = 0; index2 < arrayVector.length; index2 += 1) {
+      
+      if (arrayVector[index2] % 2 === 0) {
+        pares.push(arrayVector[index2]);
+      }
+    }
+  }
+  return pares;
+};
+
+console.log(arrayOfNumbers(vector));
