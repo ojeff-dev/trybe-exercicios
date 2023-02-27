21a982b23147207971a02e032c9301524162d8a9
Jefferson Santos <jeff-ds@outlook.com>
2023-02-27 18:58:07 -0300

    initial commit

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio-de-fixacao/src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+class Info extends React.Component {
+  render() {
+    return (
+      <div>
+        <section>
+        <h1>Jefferson Santos</h1>
+        <p>Olá, meu nome é Jefferson e sou aluno da Trybe! 😄</p>
+        </section>
+
+        <section>
+          <h2>Minhas Habilidades - Hard Skills</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Info;
